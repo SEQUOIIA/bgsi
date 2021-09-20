82d0a88f48d2a5e49c64f8e4fd43ced4be7a181c
Emil H. Clausen <sequoiia@hummel.yt>
2021-09-20 21:43:48 +0200

    Add additional GitHub Push event test data

--- src/test/handler_test.rs
+++ src/test/handler_test.rs
@@ -6,6 +6,7 @@ use crate::test::data::*;
 #[test]
 fn test_handle() {
     let data = Arc::new(Data::new_from_bytes(RULES_TEST_DATA.as_bytes(), RECEIVERS_TEST_DATA.as_bytes(), SUPPLIERS_TEST_DATA.as_bytes()));
-    handle("placeholder".to_owned(), data, GITHUB_PUSH_WEBHOOK_PAYLOAD.to_string().into_bytes()).unwrap();
+    handle("placeholder".to_owned(), data.clone(), GITHUB_PUSH_WEBHOOK_PAYLOAD.to_string().into_bytes()).unwrap();
+    handle("placeholder".to_owned(), data, GITHUB_PUSH_WEBHOOK_PAYLOAD_2.to_string().into_bytes()).unwrap();
 }
 
